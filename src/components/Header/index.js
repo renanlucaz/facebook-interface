742d6f6a3f0b3023f02b27bcfc313b752c528745
Renan Lucas <renanluquinhas53@gmail.com>
2020-04-05 18:54:16 -0300

    Profile add in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,10 +5,19 @@ import FaceLogo from '../../assets/logo.png';
 import './styles.css';
 
 function Header() {
+  const avatar = 'https://avatars1.githubusercontent.com/u/52937668?v=4';
+
   return (
+    
     <header>
       <img src={FaceLogo} alt="Facebook logo" />
+
+      <div className="profile">
+        <p>Meu perfil</p>
+        <img src={avatar} alt="User avatar"/>
+      </div>
     </header>
+    
   );
 }
 
